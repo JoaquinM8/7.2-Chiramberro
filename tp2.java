1484f8e88f5f2d6d9b743bd8cbf170e261a95014
JoaquinM8 <joaquiloquito@gmail.com>
2026-05-19 16:52:15 -0300

    19-5

--- tp2.java
+++ tp2.java
@@ -2,9 +2,9 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 
-public class tp2 extends JFrame {
+public class TP2 extends JFrame {
 
-    public tp2() {
+    public TP2() {
         setTitle("TP 2");
         setSize(720,480);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -34,7 +34,8 @@ public class tp2 extends JFrame {
         contenedor.add(inicio(), "Inicio");
 
         JPanel tituloPanel = new JPanel(new FlowLayout());
-
+        tituloPanel.setBorder(crearBordeDefault(10,10,10,10));
+        
         JLabel lblTitulo = new JLabel("Menú Principal");
         lblTitulo.setFont(new Font("Arial", Font.BOLD, 30));
 
@@ -43,7 +44,6 @@ public class tp2 extends JFrame {
         
 
         JPanel buttonPanel = new JPanel(new GridLayout(3,3,10,5));
-        buttonPanel.setBorder(crearBordeDefault(10,10,10,10));
 
         JButton ex1Button = new JButton("Detector de pares / impares");
         JButton ex2Button = new JButton("Comparador de números");
@@ -237,10 +237,10 @@ public class tp2 extends JFrame {
                 int num2 = Integer.parseInt(numField2.getText());
                 if (num1 == num2) {
                     resultLabel.setText("IGUALES");
-                    resultLabel.setForeground(Color.blue.darker());
+                    resultLabel.setForeground(Color.green.darker());
                 } else {
                     resultLabel.setText("DISTINTOS");     
-                    resultLabel.setForeground(Color.red.darker());     
+                    resultLabel.setForeground(Color.ORANGE);     
                 }
 
             } catch (NumberFormatException ex) {
@@ -343,7 +343,7 @@ public class tp2 extends JFrame {
         JPanel panel = new JPanel(new BorderLayout());
         panel.setBorder(crearBordeDefault(10, 10, 10, 10));
 
-        JLabel tituloLabel = new JLabel("Comparador 2.0", SwingConstants.CENTER);
+        JLabel tituloLabel = new JLabel("Comparador 3.0", SwingConstants.CENTER);
         tituloLabel.setFont(new Font("Arial", Font.PLAIN, 20));
 
         JPanel inputPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
@@ -426,34 +426,25 @@ public class tp2 extends JFrame {
         JPanel panel = new JPanel(new BorderLayout());
         panel.setBorder(crearBordeDefault(10, 10, 10, 10));
 
-        JLabel tituloLabel = new JLabel("Comparador 1.0", SwingConstants.CENTER);
+        JLabel tituloLabel = new JLabel("Detector Casi cero", SwingConstants.CENTER);
         tituloLabel.setFont(new Font("Arial", Font.PLAIN, 20));
 
         JPanel inputPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
-        
-            JPanel textPanel = new JPanel(new GridLayout(2,1,5,5));
 
-                JTextField numField1 = new JTextField(5);
-                numField1.setFont(new Font("Arial", Font.PLAIN, 20));
-                numField1.setHorizontalAlignment(SwingConstants.CENTER);
-                
-                JTextField numField2 = new JTextField(5);
-                numField2.setFont(new Font("Arial", Font.PLAIN, 20));
-                numField2.setHorizontalAlignment(SwingConstants.CENTER);
-
-                textPanel.add(numField1);
-                textPanel.add(numField2);
+            JTextField numField = new JTextField(5);
+            numField.setFont(new Font("Arial", Font.PLAIN, 20));
+            numField.setHorizontalAlignment(SwingConstants.CENTER);
 
             JButton calcButton = new JButton("Calcular");
 
-            inputPanel.add(textPanel);
+            inputPanel.add(numField);
             inputPanel.add(calcButton);
 
 
         JPanel resultPanel = new JPanel(new BorderLayout());
 
-            JLabel resultLabel = new JLabel("", SwingConstants.CENTER);
-            resultLabel.setFont(new Font("Arial", Font.BOLD, 75));
+            JLabel resultLabel = new JLabel("Casi cero: ", SwingConstants.CENTER);
+            resultLabel.setFont(new Font("Arial", Font.BOLD, 50));
 
             resultPanel.add(resultLabel, BorderLayout.CENTER);
             
@@ -482,18 +473,19 @@ public class tp2 extends JFrame {
 
             try {
 
-                int num1 = Integer.parseInt(numField1.getText());
-                int num2 = Integer.parseInt(numField2.getText());
-                if (num1 == num2) {
-                    resultLabel.setText("IGUALES");
-                    resultLabel.setForeground(Color.blue.darker());
+                double num = Double.parseDouble(numField.getText());
+                if (num == 0) {
+                    resultLabel.setText("Casi cero: 0");
+                    resultLabel.setForeground(Color.black);     
+                } else
+                if (-1 < num && num < 1) {
+                    resultLabel.setText("Casi cero: SI");
                 } else {
-                    resultLabel.setText("DISTINTOS");     
-                    resultLabel.setForeground(Color.red.darker());     
+                    resultLabel.setText("Casi cero: NO");        
                 }
 
             } catch (NumberFormatException ex) {
-                resultLabel.setText("");
+                resultLabel.setText("Casi cero: ");
                 JOptionPane.showMessageDialog(panel, "Error garrafal", "Error", JOptionPane.ERROR_MESSAGE);
             }
 
@@ -509,8 +501,16 @@ public class tp2 extends JFrame {
         JPanel panel = new JPanel(new BorderLayout());
         panel.setBorder(crearBordeDefault(10, 10, 10, 10));
 
-        JLabel tituloLabel = new JLabel("Comparador 1.0", SwingConstants.CENTER);
-        tituloLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+        JPanel tituloPanel = new JPanel(new BorderLayout());
+
+            JLabel tituloLabel = new JLabel("Comparador 4.0", SwingConstants.CENTER);
+            tituloLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+
+            JLabel detalleLabel = new JLabel("Con operador ternario", SwingConstants.CENTER);
+            detalleLabel.setFont(new Font("Arial", Font.ITALIC, 12));
+
+            tituloPanel.add(tituloLabel, BorderLayout.NORTH);
+            tituloPanel.add(detalleLabel, BorderLayout.SOUTH);
 
         JPanel inputPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
         
@@ -564,17 +564,10 @@ public class tp2 extends JFrame {
         calcButton.addActionListener(e -> {
 
             try {
-
                 int num1 = Integer.parseInt(numField1.getText());
                 int num2 = Integer.parseInt(numField2.getText());
-                if (num1 == num2) {
-                    resultLabel.setText("IGUALES");
-                    resultLabel.setForeground(Color.blue.darker());
-                } else {
-                    resultLabel.setText("DISTINTOS");     
-                    resultLabel.setForeground(Color.red.darker());     
-                }
-
+                String result = (num1 == num2) ? num1 + " = " + num2 : (num1 > num2) ? num1 + " > " + num2 : num1 + " < " + num2;
+                resultLabel.setText(result);
             } catch (NumberFormatException ex) {
                 resultLabel.setText("");
                 JOptionPane.showMessageDialog(panel, "Error garrafal", "Error", JOptionPane.ERROR_MESSAGE);
@@ -582,7 +575,7 @@ public class tp2 extends JFrame {
 
         });
 
-        panel.add(tituloLabel, BorderLayout.NORTH);
+        panel.add(tituloPanel, BorderLayout.NORTH);
         panel.add(centerPanel, BorderLayout.CENTER);
         return panel;
     }
@@ -597,7 +590,7 @@ public class tp2 extends JFrame {
 
         JPanel inputPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
         
-            JPanel textPanel = new JPanel(new GridLayout(2,1,5,5));
+            JPanel textPanel = new JPanel(new GridLayout(3,1,5,5));
 
                 JTextField numField1 = new JTextField(5);
                 numField1.setFont(new Font("Arial", Font.PLAIN, 20));
@@ -607,8 +600,13 @@ public class tp2 extends JFrame {
                 numField2.setFont(new Font("Arial", Font.PLAIN, 20));
                 numField2.setHorizontalAlignment(SwingConstants.CENTER);
 
+                JTextField numField3 = new JTextField(5);
+                numField3.setFont(new Font("Arial", Font.PLAIN, 20));
+                numField3.setHorizontalAlignment(SwingConstants.CENTER);
+
                 textPanel.add(numField1);
                 textPanel.add(numField2);
+                textPanel.add(numField3);
 
             JButton calcButton = new JButton("Calcular");
 
@@ -650,12 +648,36 @@ public class tp2 extends JFrame {
 
                 int num1 = Integer.parseInt(numField1.getText());
                 int num2 = Integer.parseInt(numField2.getText());
-                if (num1 == num2) {
-                    resultLabel.setText("IGUALES");
-                    resultLabel.setForeground(Color.blue.darker());
+                int num3 = Integer.parseInt(numField3.getText());
+
+                if (num1 > num2) {
+                    if (num2 > num3) {
+                        resultLabel.setText(num1 + " > " + num2 + " > " + num3);
+                    } else if (num1 == num3) {
+                        resultLabel.setText(num1 + " = " + num3 + " > " + num2);
+                    } else if (num2 == num3) {
+                        resultLabel.setText(num1 + " > " + num2 + " = " + num3);
+                    } else {
+                        resultLabel.setText(num3 + " > " + num1 + " > " + num2);
+                    }
+                } else if (num1 < num2) {
+                    if (num1 > num3) {
+                        resultLabel.setText(num2 + " > " + num1 + " > " + num3);
+                    } else if (num1 == num3) {
+                        resultLabel.setText(num2 + " > " + num1 + " = " + num3);
+                    } else if (num2 == num3) {
+                        resultLabel.setText(num2 + " = " + num3 + " > " + num1);
+                    } else {
+                        resultLabel.setText(num3 + " > " + num2 + " > " + num1);
+                    }
                 } else {
-                    resultLabel.setText("DISTINTOS");     
-                    resultLabel.setForeground(Color.red.darker());     
+                    if (num1 > num3) {
+                        resultLabel.setText(num1 + " = " + num2 + " > " + num3);
+                    } else if (num1 < num3) {
+                        resultLabel.setText(num3 + " > " + num1 + " = " + num2);
+                    } else {
+                        resultLabel.setText(num1 + " = " + num2 + " = " + num3);
+                    }
                 }
 
             } catch (NumberFormatException ex) {
@@ -679,30 +701,21 @@ public class tp2 extends JFrame {
         tituloLabel.setFont(new Font("Arial", Font.PLAIN, 20));
 
         JPanel inputPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
-        
-            JPanel textPanel = new JPanel(new GridLayout(2,1,5,5));
 
-                JTextField numField1 = new JTextField(5);
-                numField1.setFont(new Font("Arial", Font.PLAIN, 20));
-                numField1.setHorizontalAlignment(SwingConstants.CENTER);
-                
-                JTextField numField2 = new JTextField(5);
-                numField2.setFont(new Font("Arial", Font.PLAIN, 20));
-                numField2.setHorizontalAlignment(SwingConstants.CENTER);
-
-                textPanel.add(numField1);
-                textPanel.add(numField2);
+            JTextField notaField = new JTextField(5);
+            notaField.setFont(new Font("Arial", Font.PLAIN, 20));
+            notaField.setHorizontalAlignment(SwingConstants.CENTER);
 
             JButton calcButton = new JButton("Calcular");
 
-            inputPanel.add(textPanel);
+            inputPanel.add(notaField);
             inputPanel.add(calcButton);
 
 
         JPanel resultPanel = new JPanel(new BorderLayout());
 
             JLabel resultLabel = new JLabel("", SwingConstants.CENTER);
-            resultLabel.setFont(new Font("Arial", Font.BOLD, 75));
+            resultLabel.setFont(new Font("Arial", Font.BOLD, 50));
 
             resultPanel.add(resultLabel, BorderLayout.CENTER);
             
@@ -731,14 +744,26 @@ public class tp2 extends JFrame {
 
             try {
 
-                int num1 = Integer.parseInt(numField1.getText());
-                int num2 = Integer.parseInt(numField2.getText());
-                if (num1 == num2) {
-                    resultLabel.setText("IGUALES");
-                    resultLabel.setForeground(Color.blue.darker());
+                double nota = Double.parseDouble(notaField.getText());
+                
+                if (nota < 0 || nota > 10) {
+                    resultLabel.setText("");
+                    JOptionPane.showMessageDialog(panel, "La nota debe estar entre 0 y 10", "Error", JOptionPane.ERROR_MESSAGE);
+                } else if (nota < 5) {
+                    resultLabel.setText("Insuficiente");
+                    resultLabel.setForeground(Color.red.darker());
+                } else if (nota < 6) {
+                    resultLabel.setText("Suficiente");
+                    resultLabel.setForeground(Color.ORANGE);
+                } else if (nota < 7) {
+                    resultLabel.setText("Bueno");
+                    resultLabel.setForeground(Color.green);
+                } else if (nota < 9) {
+                    resultLabel.setText("Notable");
+                    resultLabel.setForeground(Color.green.darker());
                 } else {
-                    resultLabel.setText("DISTINTOS");     
-                    resultLabel.setForeground(Color.red.darker());     
+                    resultLabel.setText("Sobresaliente");
+                    resultLabel.setForeground(Color.cyan.darker());
                 }
 
             } catch (NumberFormatException ex) {
@@ -758,23 +783,40 @@ public class tp2 extends JFrame {
         JPanel panel = new JPanel(new BorderLayout());
         panel.setBorder(crearBordeDefault(10, 10, 10, 10));
 
-        JLabel tituloLabel = new JLabel("Comparador 1.0", SwingConstants.CENTER);
+        JLabel tituloLabel = new JLabel("Validar fecha", SwingConstants.CENTER);
         tituloLabel.setFont(new Font("Arial", Font.PLAIN, 20));
 
         JPanel inputPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
         
-            JPanel textPanel = new JPanel(new GridLayout(2,1,5,5));
+            JPanel textPanel = new JPanel(new GridLayout(3,2,5,5));
 
-                JTextField numField1 = new JTextField(5);
-                numField1.setFont(new Font("Arial", Font.PLAIN, 20));
-                numField1.setHorizontalAlignment(SwingConstants.CENTER);
-                
-                JTextField numField2 = new JTextField(5);
-                numField2.setFont(new Font("Arial", Font.PLAIN, 20));
-                numField2.setHorizontalAlignment(SwingConstants.CENTER);
+                JLabel diaLabel = new JLabel("Día:", SwingConstants.CENTER);
+                diaLabel.setFont(new Font("Arial", Font.PLAIN, 14));
 
-                textPanel.add(numField1);
-                textPanel.add(numField2);
+                JLabel mesLabel = new JLabel("Mes:", SwingConstants.CENTER);
+                mesLabel.setFont(new Font("Arial", Font.PLAIN, 14));
+
+                JLabel añoLabel = new JLabel("Año:", SwingConstants.CENTER);
+                añoLabel.setFont(new Font("Arial", Font.PLAIN, 14));
+
+                JTextField diaField = new JTextField(5);
+                diaField.setFont(new Font("Arial", Font.PLAIN, 15));
+                diaField.setHorizontalAlignment(SwingConstants.CENTER);
+
+                JTextField mesField = new JTextField(5);
+                mesField.setFont(new Font("Arial", Font.PLAIN, 15));
+                mesField.setHorizontalAlignment(SwingConstants.CENTER);
+
+                JTextField añoField = new JTextField(5);
+                añoField.setFont(new Font("Arial", Font.PLAIN, 15));
+                añoField.setHorizontalAlignment(SwingConstants.CENTER);
+
+                textPanel.add(diaLabel);
+                textPanel.add(diaField);
+                textPanel.add(mesLabel);
+                textPanel.add(mesField);
+                textPanel.add(añoLabel);
+                textPanel.add(añoField);
 
             JButton calcButton = new JButton("Calcular");
 
@@ -785,7 +827,7 @@ public class tp2 extends JFrame {
         JPanel resultPanel = new JPanel(new BorderLayout());
 
             JLabel resultLabel = new JLabel("", SwingConstants.CENTER);
-            resultLabel.setFont(new Font("Arial", Font.BOLD, 75));
+            resultLabel.setFont(new Font("Arial", Font.BOLD, 20));
 
             resultPanel.add(resultLabel, BorderLayout.CENTER);
             
@@ -814,15 +856,28 @@ public class tp2 extends JFrame {
 
             try {
 
-                int num1 = Integer.parseInt(numField1.getText());
-                int num2 = Integer.parseInt(numField2.getText());
-                if (num1 == num2) {
-                    resultLabel.setText("IGUALES");
-                    resultLabel.setForeground(Color.blue.darker());
+                int dia = Integer.parseInt(diaField.getText());
+                int mes = Integer.parseInt(mesField.getText());
+                int año = Integer.parseInt(añoField.getText());
+
+                if (mes == 2 && dia > 28) {
+                    resultLabel.setText("Febrero no puede tener más de 28 días.");
+                    resultLabel.setFont(new Font("Arial", Font.ITALIC, 15));
+                    resultLabel.setForeground(Color.red);
+                } else if ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && dia > 30) {
+                    resultLabel.setText("El mes seleccionado no puede tener más de 30 días.");
+                    resultLabel.setFont(new Font("Arial", Font.ITALIC, 15));
+                    resultLabel.setForeground(Color.red);
+                } else if (dia < 1 || dia > 31 || mes < 1 || mes > 12) {
+                    resultLabel.setText("Día o mes fuera de rango.");
+                    resultLabel.setFont(new Font("Arial", Font.ITALIC, 15));
+                    resultLabel.setForeground(Color.red);
                 } else {
-                    resultLabel.setText("DISTINTOS");     
-                    resultLabel.setForeground(Color.red.darker());     
+                    resultLabel.setText("Fecha válida: " + dia + "/" + mes + "/" + año);
+                    resultLabel.setFont(new Font("Arial", Font.BOLD, 25));
+                    resultLabel.setForeground(Color.black);
                 }
+
 
             } catch (NumberFormatException ex) {
                 resultLabel.setText("");
@@ -836,10 +891,8 @@ public class tp2 extends JFrame {
         return panel;
     }
 
-
-
     public static void main(String[] args) {
-    SwingUtilities.invokeLater(() -> new tp2());
+    SwingUtilities.invokeLater(() -> new TP2());
     }
 
 }
